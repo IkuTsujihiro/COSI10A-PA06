f108f86f8ec6713616a943e47adce880b551f894
lsakai <62360817+lsakai@users.noreply.github.com>
2020-04-10 22:30:40 -0400

    Updated the python script with code from the jupyter notebook

--- pa06_hangman/hangman_app.py
+++ pa06_hangman/hangman_app.py
@@ -17,7 +17,7 @@ def generate_random_word():
       word = ""
       for line in word_list:
          if count == rand_num:
-            word = line
+            word = line[:-1]
             break
          count += 1
 
@@ -25,20 +25,80 @@ def generate_random_word():
 
 def print_word(word, guessed_letters):
    """ given a word and a list of guessed letters, print - for every letter that hasn't been guessed in word """
-    result = ""
-    for letter in word:
-        if letter in guessed_letters:
-            result += letter
-        else:
-            result += '-'
-    print(result)
+   result = ""
+   for letter in word:
+      if letter in guessed_letters:
+         result += letter
+      else:
+         result += '-'
+   print(result)
+
+
+def correct_guesses(word, guessed_letters):
+   correct_letters = 0
+   for letter in word:
+      if letter in guessed_letters:
+         correct_letters += 1
+   return correct_letters
+
 
 def play_hangman():
    """ this is the python script version of the game """
-   print("The hangman app is under construction. Try again later!")
+   want_to_play = True
+   print("Welcome to the hangman game!")
+   while (want_to_play):
+      guessed_letters = []
+      guesses_left = 6
+      print("\nStarting new game...\n")
+      print("You have", guesses_left, "guesses.")
+      word = generate_random_word()
+      letter = input("Guess a letter! ")
+      done = False
+      while not done:
+         if letter in guessed_letters:
+               ##"subtract one from guesses_left"
+               guesses_left = guesses_left - 1
+               ##"and tell them they already guessed that letter"
+               print("This letter has already been guessed.")
+               
+         elif letter not in word:
+               ##"add letter to guessed letters"
+               guessed_letters.append(letter)
+               ##"tell user the letter is not in the word"
+               print("That letter is not in the word.")
+               ##"subtract one from the guesses_left"
+               guesses_left = guesses_left - 1
+         else:
+               ##"add letter to guessed letters"
+               guessed_letters.append(letter)
+               ##"tell user the letter is in the word"
+               print("The letter is in the word.")
+         
+         if correct_guesses(word, guessed_letters) == len(word):
+               done = True
+               print("You have correctly guessed the word. Congrats!")
+               print("The word is:", word)
+               ##"all the letters in the word have been guessed":
+               ##"set done to be true and tell the user they won!"
+         
+         ##" elif the number of guesses left is zero"
+         elif guesses_left == 0:
+               ##"set done to be true and tell the user they lost!"
+               done = True
+               print("You have no more guesses, you have lost the game!")
+               print("The word was:", word)
+         else:
+               print_word(word,guessed_letters)
+               print("You have", guesses_left, "guesses.")
+               letter = input("Please guess another letter. ")
+      answer = input("Would you like to play again? (y/n) ")
+      if answer == 'y':
+         want_to_play = True
+      else:
+         want_to_play = False
+         print("Thank you for playing!")
+
+
 
 if __name__ == '__main__':
     play_hangman()
-
-#This is a test
-print(generate_random_word())
